944a4bf40aba78898e491f2d27284abf8cce83b7
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-04-19 23:28:01 -0300

    Update ex1.py

--- atividade4/ex1.py
+++ atividade4/ex1.py
@@ -1,1 +1,25 @@
+def contar_caracteres(frase):
+  
+  contagem = {}
+
+ 
+  for caractere in frase:
+     
+      if caractere in contagem:
+         
+          contagem[caractere] += 1
+      else:
+         
+          contagem[caractere] = 1
+
+  return contagem
+
+
+frase = input("Insira uma frase: ")
+
+
+resultado = contar_caracteres(frase)
+
+
+print(resultado)
 
