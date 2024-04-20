487f6dd2c90abf34d1775f1e9c21254c04b4360d
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-04-19 23:43:32 -0300

    Update ex13.py

--- atividade4/ex13.py
+++ atividade4/ex13.py
@@ -1,1 +1,29 @@
 
+sandwich_orders = []
+
+
+while True:
+    pedido = input("Digite o nome do sanduíche que deseja (ou '1' para encerrar): ")
+    if pedido.lower() == '1':
+        break
+    else:
+        sandwich_orders.append(pedido)
+
+
+finished_sandwiches = []
+
+
+for pedido in sandwich_orders:
+    print(f"Preparei seu sanduíche de {pedido}.")
+
+    
+    finished_sandwiches.append(pedido)
+
+
+print("\nTodos os sanduíches foram preparados!\n")
+
+
+print("Sanduíches preparados:")
+for sanduiche in finished_sandwiches:
+    print(sanduiche)
+
