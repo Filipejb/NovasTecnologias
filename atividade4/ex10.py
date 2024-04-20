d86ca64d35fb7b3ef9700ce62a8f0e98f7bbb6c4
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-04-19 23:37:12 -0300

    Update ex10.py

--- atividade4/ex10.py
+++ atividade4/ex10.py
@@ -1,1 +1,20 @@
 
+first_name = input("Digite o primeiro nome: ")
+last_name = input("Digite o sobrenome: ")
+age = int(input("Digite a idade: "))
+city = input("Digite a cidade: ")
+
+
+pessoa = {
+    'first_name': first_name,
+    'last_name': last_name,
+    'age': age,
+    'city': city
+}
+
+
+print("Primeiro nome:", pessoa['first_name'])
+print("Sobrenome:", pessoa['last_name'])
+print("Idade:", pessoa['age'])
+print("Cidade:", pessoa['city'])
+
