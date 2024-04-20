c55ce8c2cf9f33ed673be670f1fd9de6a2fd5338
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-04-19 23:38:41 -0300

    Update ex12.py

--- atividade4/ex12.py
+++ atividade4/ex12.py
@@ -1,1 +1,16 @@
 
+animal1 = {'nome': 'Max', 'tipo': 'Cachorro', 'dono': 'Maria'}
+animal2 = {'nome': 'Mittens', 'tipo': 'Gato', 'dono': 'João'}
+animal3 = {'nome': 'Bella', 'tipo': 'Cachorro', 'dono': 'Ana'}
+animal4 = {'nome': 'Whiskers', 'tipo': 'Coelho', 'dono': 'Pedro'}
+
+
+pets = [animal1, animal2, animal3, animal4]
+
+
+for pet in pets:
+    print(f"Nome: {pet['nome']}")
+    print(f"Tipo: {pet['tipo']}")
+    print(f"Dono: {pet['dono']}")
+    print() 
+
